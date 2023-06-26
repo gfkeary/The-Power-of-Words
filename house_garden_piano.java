2bbe90b3901e8338b4d4340b8bb5c8eb33695747
gfkeary <136633914+gfkeary@users.noreply.github.com>
2023-06-26 22:23:53 +0700

    Fix UI layout issues

---
+++ house_garden_piano.java
@@ -0,0 +1,73 @@
+// The Power of Words: Java program to illustrate the power of words 
+
+public class PowerOfWords {
+	public static void main(String[] args) { 
+   
+		// Declaring some of the most powerful words 
+		String love = "Love"; 
+		String peace = "Peace"; 
+		String destiny = "Destiny"; 
+		String faith = "Faith";
+		String success = "Success"; 
+		
+		// Some of the most powerful messages
+		String message1 = "The power of " + love + " can conquer all"; 
+		String message2 = "No one can achieve true " + peace + " without understanding"; 
+		String message3 = "Your future is in your own hands, it is your " + destiny + " to decide"; 
+		String message4 = "It is only with " + faith + " that you can reach your goals"; 
+		String message5 = "Nothing is more powerful than the striving for " + success + ""; 
+		
+		// Printing the messages
+		System.out.println(message1); 
+		System.out.println(message2); 
+		System.out.println(message3); 
+		System.out.println(message4); 
+		System.out.println(message5); 
+		
+		// Declaring powerful adjectives
+		String strong = "Strong"; 
+		String brave = "Brave"; 
+		String wise = "Wise";
+		String determined = "Determined"; 
+		String passionate = "Passionate"; 
+		
+		// Some of the most powerful messages with adjectives
+		String message6 = "In order to be truly " + strong + " you must accept your weaknesses"; 
+		String message7 = "To be successful you must be " + brave + " enough to take risks"; 
+		String message8 = "Learn to be " + wise + " in order to make sound decisions";
+		String message9 = "You will never succeed unless you are " + determined + " in your ambition"; 
+		String message10 = "The key to success is to be " + passionate + " about your work"; 
+		
+		// Printing the messages
+		System.out.println(message6); 
+		System.out.println(message7); 
+		System.out.println(message8); 
+		System.out.println(message9); 
+		System.out.println(message10); 
+		
+		// Declaring powerful verbs
+		String inspire = "Inspire"; 
+		String motivate = "Motivate"; 
+		String create = "Create"; 
+		String strive = "Strive"; 
+		String believe = "Believe"; 
+		
+		// Some of the most powerful messages with verbs
+		String message11 = "You must use your words to " + inspire + " others"; 
+		String message12 = "Speak to " + motivate + " those around you"; 
+		String message13 = "Decide to " + create + " a better future"; 
+		String message14 = "Always " + strive + " to achieve your goals"; 
+		String message15 = "Never give up, " + believe + " in yourself"; 
+		
+		// Printing the messages
+		System.out.println(message11); 
+		System.out.println(message12); 
+		System.out.println(message13); 
+		System.out.println(message14); 
+		System.out.println(message15); 
+		
+		// The power of words is remarkable 
+		String message16 = "It is astonishing what kind of impact words can have"; 
+		System.out.println(message16); 
+	} 
+}
